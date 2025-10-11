97dda556ad80a0a5f0038a0690d2ca8b67565050
javid shabin <javid.main.act@gmail.com>
2025-10-11 09:57:27 +0530

    Make anoter change for deploying

--- index.js
+++ index.js
@@ -6,34 +6,31 @@ import cors from "cors";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import { connectDb } from "./src/configs/dbConnection.js";
-import { errorHandler } from "./src/middlewares/ErrorHandler.js";
+import { errorHandler } from "./src/middlewares/errorHandler.js";
 import apiRoutes from "./src/api.js";
-
-const PORT = 5000;
 
 const server = express();
 
+// Middlewares
 server.use(cors());
-
 server.use(cookieParser());
 server.use(helmet());
 server.use(express.json());
 
-server.use(errorHandler);
-
+// Routes
 server.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
 server.use("/api", apiRoutes);
 
+// Global error handler
+server.use(errorHandler);
+
+// Connect to DB (Vercel runs cold starts, so ensure this is async safe)
 connectDb()
-  .then(() => {
-    console.log("Connected to MongoDB");
-    server.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error("MongoDB connection error:", error));
+
+// ❌ No server.listen() here!
+export default server;
