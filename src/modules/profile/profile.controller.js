0cc36ca76da2760647c3dc3a35670987f011e4c5
javid shabin <javid.main.act@gmail.com>
2025-10-18 00:06:38 +0530

    Create the otp generating for update password, and verify and passowrd function

--- src/modules/profile/profile.controller.js
+++ src/modules/profile/profile.controller.js
@@ -1,11 +1,11 @@
-import { userProfileService } from "./profile.service.js";
+import { updatePassowrdService, updatePasswordOtpService, userProfileService } from "./profile.service.js";
 
 export const getUserProfile = async (req, res, next) => {
   try {
     const { id } = req.user;
 
     // Pass the id as an object since service expects { id }
-    const result = await userProfileService( id );
+    const result = await userProfileService(id);
 
     // Correct JSON response format
     res.status(200).json({
@@ -21,10 +21,29 @@ export const getUserProfile = async (req, res, next) => {
 
 export const updateProfile = async (req, res, next) => {
   try {
-  } catch (error) {}
+  } catch (error) { }
 };
 
 export const getAllUsers = async (req, res, next) => {
   try {
-  } catch (error) {}
+  } catch (error) { }
 };
+
+export const updatePasswordOtp = async (req, res, next) => {
+  try {
+    const result = await updatePasswordOtpService(req.body)
+    res.status(201).json(result);
+  } catch (error) {
+    // Forward error to error-handling middleware
+    next(error);
+  }
+}
+
+export const updatePassowrd = async (req, res, next) => {
+  try {
+    const result = await updatePassowrdService(req.body)
+    res.status(200).json(result)
+  } catch (error) {
+    next(error)
+  }
+}
